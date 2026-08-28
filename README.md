4b80695d214eb4eda1194b452448ff8ddf619def
Nana Yaw Oti-Antwi <notiantwi@gmail.com>
2026-08-28 14:24:41 +0000

    feat : Add Search Feature

---
+++ README.md
@@ -0,0 +1,5 @@
+# Library Companion
+
+This is your friendly neighbourhood library database designed to track new books and search for book to know their stataus in a healthy Command-Line Interface
+
+Very nice and friendly!
--- library.py
+++ library.py
@@ -10,25 +10,45 @@ def allbooks ():
     for index, c in enumerate(booklist, start=1):  
        print (f"{index}. {c["title"]}--{c["author"]}--{c["status"]}")
 
+def searchcbook(query):
+    found = False
+    for p in booklist:
+        if query == p["title"]:
+            print (p)
+        if query.lower() in p["title"].lower():
+            print(f"Found: {p["title"]} -- {p["author"]} -- {p["status"]}")
+            found = True
+            
+    if not found:
+        print("No contacts matched your search.")
 def main ():
     ui = """
     Library Database
     1. Add Book
     2. View All Books
+    3. Search For A Book
+    0. Exit
     """
 
     print (ui)
     print()
-    choice = input("Choose a number to navigate or type in 'exit' to quit: ").lower().strip()
+    choice = input("Choose a number to navigate or type in 'exit' to quit: ").strip()
     if choice == "1":
         print()
         addbook()
+
     elif choice == "2":
         print()
         allbooks()
-    elif choice == "exit":
+
+    elif choice == "3":
+        print()
+        searchcbook()
+
+    elif choice == "0":
         print()
         print("Goodbye!")
+        
     else:
         print("Invalid Choice!")
 main() 
