a358d37be0ee3a5502fa9b1e8a2bb4691c4b06e7
Nana Yaw Oti-Antwi <notiantwi@gmail.com>
2026-08-28 20:20:45 +0000

    feat : Add Borrow Book Feature

--- library.py
+++ library.py
@@ -2,31 +2,46 @@ booklist = []
 def addbook ():
     title = input("Enter Book Title: ")
     author = input("Enter Author Name: ")
-    status = "Available"
+    status = "Avialable"
     booklist.append({"title":title, "author":author, "status":status})
     print (f"{title} added successfully!")
 
 def allbooks ():
     for index, c in enumerate(booklist, start=1):  
-       print (f"{index}. {c["title"]}--{c["author"]}--{c["status"]}")
+        print (f"{index}. {c['title']}--{c['author']}--{c['status']}")
+       
 
-def searchcbook(query):
+def searchbook(query):
     found = False
     for p in booklist:
         if query == p["title"]:
             print (p)
         if query.lower() in p["title"].lower():
-            print(f"Found: {p["title"]} -- {p["author"]} -- {p["status"]}")
+            print(f"Found: {p['title']} -- {p['author']} -- {p['status']}")
             found = True
+        if not found:
+                print("No contacts matched your search.")
+
+def borrowbooks():
+    borrow = input("Enter the book to be borrowed: ")
+
+    for u in booklist:
+        if u["title"] == borrow:
+            u["status"] = "Borrowed"
+
+            print("Book Borrowed.")
+            return
+    print(f"'{borrow}' not found in the list.")
             
-    if not found:
-        print("No contacts matched your search.")
+
+    
 def main ():
     ui = """
     Library Database
     1. Add Book
     2. View All Books
     3. Search For A Book
+    4. Borrow Book
     0. Exit
     """
 
@@ -43,12 +58,16 @@ def main ():
 
     elif choice == "3":
         print()
-        searchcbook()
+        searchbook()
+        
+    elif choice == "4":
+        print()
+        borrowbooks()
 
     elif choice == "0":
         print()
         print("Goodbye!")
-        
+
     else:
         print("Invalid Choice!")
 main() 
